0af2220239d38edb1e5138fba255f9091493c869
Lauane Pasquini <lauanepasquine8@gmail.com>
2026-05-26 16:44:49 -0300

    ok

--- sistemaMVC/src/routes/agendamentoRoutes.js
+++ sistemaMVC/src/routes/agendamentoRoutes.js
@@ -1,8 +1,5 @@
 const express = require('express');
 const router = express.Router();
-
 const agendamentoController = require('../controllers/agendamentoController');
-
 router.post('/agendamento', agendamentoController.inserirAgendamento);
-
 module.exports = router;
